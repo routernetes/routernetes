beb6544f9911f3c9d6efb148735555afc673b33b
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2022-06-09 09:31:24 -0600

    better wait

--- k3s/deploy.sh
+++ k3s/deploy.sh
@@ -6,5 +6,5 @@ sudo k3s kubectl apply -f firewalld-configmap.yaml
 sudo k3s kubectl apply -f zincati-configmap.yaml
 sudo k3s kubectl apply -f daemonset.yaml
 sudo k3s kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/latest/download/system-upgrade-controller.yaml
-sleep 10
+sudo k3s kubectl wait deployment -n system-upgrade system-upgrade-controller --for condition=Available=True
 sudo k3s kubectl apply -f upgrade-plan.yaml
