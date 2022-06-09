72a82d0c169034210eb2e7189bfac9d88076095b
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2022-06-09 09:32:50 -0600

    pvc own file

--- k3s/deploy.sh
+++ k3s/deploy.sh
@@ -4,6 +4,7 @@ sudo k3s kubectl apply -f namespace.yaml
 sudo k3s kubectl apply -f dnsmasq-configmap.yaml
 sudo k3s kubectl apply -f firewalld-configmap.yaml
 sudo k3s kubectl apply -f zincati-configmap.yaml
+sudo k3s kubectl apply -f dhcp-pvc.yaml
 sudo k3s kubectl apply -f daemonset.yaml
 sudo k3s kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/latest/download/system-upgrade-controller.yaml
 sudo k3s kubectl wait deployment -n system-upgrade system-upgrade-controller --for condition=Available=True
