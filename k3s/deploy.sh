b8f6258b8004470bca2e12fc183758a65c1cfa32
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2022-06-10 10:39:24 -0600

    moved to operator

--- k3s/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo kubectl apply -f namespace.yaml
-sudo kubectl apply -f dnsmasq-configmap.yaml
-sudo kubectl apply -f firewalld-configmap.yaml
-sudo kubectl apply -f zincati-configmap.yaml
-sudo kubectl apply -f dhcp-pvc.yaml
-sudo kubectl apply -f daemonset.yaml
