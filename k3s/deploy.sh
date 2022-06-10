a098c0435f59ec5b7f31501df4a8cbb7a4e7f9c4
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2022-06-10 07:35:03 -0600

    auto install deps

--- k3s/deploy.sh
+++ k3s/deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-sudo k3s kubectl apply -f namespace.yaml
-sudo k3s kubectl apply -f dnsmasq-configmap.yaml
-sudo k3s kubectl apply -f firewalld-configmap.yaml
-sudo k3s kubectl apply -f zincati-configmap.yaml
-sudo k3s kubectl apply -f dhcp-pvc.yaml
-sudo k3s kubectl apply -f daemonset.yaml
-sudo k3s kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/latest/download/system-upgrade-controller.yaml
-sudo k3s kubectl wait deployment -n system-upgrade system-upgrade-controller --for condition=Available=True
-sudo k3s kubectl apply -f upgrade-plan.yaml
+sudo kubectl apply -f namespace.yaml
+sudo kubectl apply -f dnsmasq-configmap.yaml
+sudo kubectl apply -f firewalld-configmap.yaml
+sudo kubectl apply -f zincati-configmap.yaml
+sudo kubectl apply -f dhcp-pvc.yaml
+sudo kubectl apply -f daemonset.yaml
