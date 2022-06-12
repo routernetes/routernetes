69a34a80093938cce685169965c9c3ef3e1cbc04
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2022-06-11 21:27:19 -0600

    create_iso.sh

--- create_iso.sh
+++ create_iso.sh
@@ -1,9 +1,9 @@
-```
+#!/bin/bash
 coreos-installer download -f iso
 butane --pretty --strict -o config.ign butane.yaml
 coreos-installer iso customize \
     --dest-device /dev/sda \
     --dest-ignition config.ign \
-    -o custom.iso fedora-coreos-36.20220522.3.0-live.x86_64.iso
-rm config.ign
-```
+    -o routernetes.iso fedora-coreos-*-live.x86_64.iso
+rm -f config.ign
+rm -f fedora-coreos-*-live.x86_64.iso*
