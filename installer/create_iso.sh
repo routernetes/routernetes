c65864f67b3bf09b3cf8e397e63e994bec31720f
koushik-ms <koushik.ms@gmail.com>
2022-06-24 17:17:07 +0000

    add butane.yaml to create-iso image

--- installer/create_iso.sh
+++ installer/create_iso.sh
@@ -3,7 +3,7 @@ set -e
 
 rm -f routernetes.iso
 coreos-installer download -f iso -C /tmp
-butane --pretty --strict -o /tmp/config.ign butane.yaml
+butane --pretty --strict -o /tmp/config.ign /etc/butane.yaml
 coreos-installer iso customize \
     --dest-device /dev/sda \
     --dest-ignition /tmp/config.ign \
